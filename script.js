56841ea3c944da9ae032d95dba96dfcbd3b44771
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-15 14:32:35 -0700

    Create operate function

--- script.js
+++ script.js
@@ -14,6 +14,29 @@ function divide(a, b) {
   return a / b;
 }
 
-var firstNumber = 0;
-var operator = add(1, 1);
-var secondNumber = 0;
+let firstNumber = 4;
+let operator = "-";
+let secondNumber = 3;
+let result = 0;
+
+function operate() {
+  switch (operator) {
+    case "+":
+      result = add(firstNumber, secondNumber);
+      break;
+    case "-":
+      result = subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      result = multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      result = divide(firstNumber, secondNumber);
+      break;
+    default:
+      result = "Invalid operation";
+  }
+  return result;
+}
+
+console.log("Result:", operate());
