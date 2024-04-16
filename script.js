363df4a445b4d64a23605eb3b4ef992c5a2715c4
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-15 22:39:54 -0700

    Finish logic

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b;
+  return b === 0 ? "Not possible" : a / b;
 }
 
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
 let result = 0;
+let usedAdd = 0;
 
 function operate() {
   switch (operator) {
@@ -41,33 +42,8 @@ function operate() {
 
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
-
-// buttons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     display.value += button.textContent;
-
-//     if (button.textContent === "C") {
-//       display.value = "";
-//     }
-
-//     if (
-//       button.textContent === "+" ||
-//       button.textContent === "-" ||
-//       button.textContent === "*" ||
-//       button.textContent === "/"
-//     ) {
-//       firstNumber = parseInt(display.value.slice(0, -1));
-//       operator = button.textContent;
-//     }
-
-//     if (button.textContent === "=") {
-//       secondNumber = parseInt(
-//         display.value.slice(firstNumber.toString().length + 1, -1)
-//       );
-//       display.value = operate();
-//     }
-//   });
-// });
+let displayNumber = 0;
+let total = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -76,23 +52,49 @@ buttons.forEach((button) => {
     switch (button.textContent) {
       case "C":
         display.value = "";
+        firstNumber = 0;
+        operator = "";
+        secondNumber = 0;
+        displayNumber = 0;
+        usedAdd = 0;
         break;
 
       case "+":
       case "-":
       case "*":
       case "/":
-        firstNumber = parseInt(display.value.slice(0, -1));
         operator = button.textContent;
+        if (usedAdd === 0) {
+          firstNumber = parseInt(displayNumber);
+          displayNumber = 0;
+          usedAdd = 1;
+        } else {
+          secondNumber = parseInt(displayNumber);
+          displayNumber = 0;
+          total = operate();
+          firstNumber = total;
+          display.value = firstNumber + operator;
+        }
         break;
+
       case "=":
-        secondNumber = parseInt(
-          display.value.slice(firstNumber.toString().length + 1, -1)
-        );
-        display.value = operate();
+        if (operator === "") {
+          display.value = parseInt(displayNumber);
+        } else {
+          secondNumber = parseInt(displayNumber);
+          total = operate();
+          display.value = total;
+          firstNumber = 0;
+          secondNumber = 0;
+          operator = "";
+          displayNumber = total;
+          usedAdd = 0;
+        }
+
         break;
 
       default:
+        displayNumber += button.textContent;
         break;
     }
   });
